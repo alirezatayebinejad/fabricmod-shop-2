40a495a9f641946b5eb36bb5fdbfa8cbfe5ac9bd
Mehdi salehi <mehdi.salehi021@gmail.com>
2025-08-09 15:36:44 +0330

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
         hostname: "fabric.web-developers.shop",
       },
       {
+        protocol: "http",
+        hostname: "87.248.156.50",
+        port:8000
+      },
+      {
         protocol: "https",
         hostname: "trustseal.enamad.ir",
       },
