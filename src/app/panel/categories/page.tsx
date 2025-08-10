db5104baf1508d4a0eb5a922c33c85acc14742eb
Mehdi salehi <mehdi.salehi021@gmail.com>
2025-08-11 02:05:34 +0330

    fix build error

--- src/app/panel/categories/page.tsx
+++ src/app/panel/categories/page.tsx
@@ -1,15 +1,20 @@
+"use client";
+
 import HeaderCategories from "@/app/panel/categories/_components/HeaderCategories";
 import TableCategories from "@/app/panel/categories/_components/TableCategories";
 import Breadcrumb from "@/components/datadisplay/Breadcrumb";
+import { useState } from "react";
 
 export default async function CategoriesPage() {
+    const [type, setType] = useState<"post" | "product">("post"); // Set a default type
+
   return (
     <main>
       <div className="pages_wrapper">
         <div className="mb-5">
           <Breadcrumb items={[{ title: "پنل" }, { title: "دسته بندی ها" }]} />
         </div>
-        <HeaderCategories />
+        <HeaderCategories type={type} setType={setType} />
         <TableCategories />
       </div>
     </main>
